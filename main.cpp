27243dae9c7a321322b6ef3f365f5a8103c31f7f
xing <52873782+LZX842056112@users.noreply.github.com>
2025-10-10 15:55:02 +0800

    修改

--- main.cpp
+++ main.cpp
@@ -1,48 +1,48 @@
 #include <iostream>
 
+// day01
+// ******************************************************
 // void f1(){}
+// int main() {
 //
-int main() {
-    // day01
-    // ******************************************************
-    // printf("Hello World!\n");
-    // std::cout << "Hello World!\n";
-    // std::cout << "Hello World!" << std::endl;
-    // std::cout << "**************************" << std::endl;
-    //
-    // int a = 1;
-    // printf("%d\n", a);
-    // std::cout << sizeof(a) << std::endl;
-    // std::cout << "**************************" << std::endl;
-    //
-    // char b = 'a';
-    // std::cout << sizeof(b) << std::endl;
-    // std::cout << "**************************" << std::endl;
-    //
-    // float c = 1.1;
-    // std::cout << sizeof(c) << std::endl;
-    // std::cout << "**************************" << std::endl;
-    //
-    // double d = 1.1;
-    // std::cout << sizeof(d) << std::endl;
-    // std::cout << "**************************" << std::endl;
-    //
-    // bool e = true;
-    // std::cout << sizeof(e) << std::endl;
-    // std::cout << "**************************" << std::endl;
-    //
-    // int a = 6;
-    // int b = 0;
-    // std::cout << a / b << std::endl;
-    //
-    // short int a = 2;
-    // std::cout << sizeof a << std::endl;
-    //
-    // long int b = 2;
-    // std::cout << sizeof b << std::endl;
-    //
-    // long long int c = 2;
-    // std::cout << sizeof c << std::endl;
-
-    return 0;
-}
+//     printf("Hello World!\n");
+//     std::cout << "Hello World!\n";
+//     std::cout << "Hello World!" << std::endl;
+//     std::cout << "**************************" << std::endl;
+//
+//     int a = 1;
+//     printf("%d\n", a);
+//     std::cout << sizeof(a) << std::endl;
+//     std::cout << "**************************" << std::endl;
+//
+//     char b = 'a';
+//     std::cout << sizeof(b) << std::endl;
+//     std::cout << "**************************" << std::endl;
+//
+//     float c = 1.1;
+//     std::cout << sizeof(c) << std::endl;
+//     std::cout << "**************************" << std::endl;
+//
+//     double d = 1.1;
+//     std::cout << sizeof(d) << std::endl;
+//     std::cout << "**************************" << std::endl;
+//
+//     bool e = true;
+//     std::cout << sizeof(e) << std::endl;
+//     std::cout << "**************************" << std::endl;
+//
+//     int a = 6;
+//     int b = 0;
+//     std::cout << a / b << std::endl;
+//
+//     short int a = 2;
+//     std::cout << sizeof a << std::endl;
+//
+//     long int b = 2;
+//     std::cout << sizeof b << std::endl;
+//
+//     long long int c = 2;
+//     std::cout << sizeof c << std::endl;
+//
+//     return 0;
+// }
